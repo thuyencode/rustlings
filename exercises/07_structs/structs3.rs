3cb1d35e564b360f56760943d2207eac1c582e62
Thuyen Code <thuyencode@proton.me>
2024-03-03 15:56:38 +0700

    Complete the 08's exercises

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -7,82 +7,80 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
-    sender_country: String,
-    recipient_country: String,
-    weight_in_grams: u32,
+  sender_country: String,
+  recipient_country: String,
+  weight_in_grams: u32,
 }
 
 impl Package {
-    fn new(sender_country: String, recipient_country: String, weight_in_grams: u32) -> Package {
-        if weight_in_grams < 10 {
-            // This is not how you should handle errors in Rust,
-            // but we will learn about error handling later.
-            panic!("Can not ship a package with weight below 10 grams.")
-        } else {
-            Package {
-                sender_country,
-                recipient_country,
-                weight_in_grams,
-            }
-        }
+  fn new(sender_country: String, recipient_country: String, weight_in_grams: u32) -> Package {
+    if weight_in_grams < 10 {
+      // This is not how you should handle errors in Rust,
+      // but we will learn about error handling later.
+      panic!("Can not ship a package with weight below 10 grams.")
+    } else {
+      Package {
+        sender_country,
+        recipient_country,
+        weight_in_grams,
+      }
     }
+  }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
-    }
+  fn is_international(&self) -> bool {
+    self.sender_country != self.recipient_country
+  }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
-    }
+  fn get_fees(&self, cents_per_gram: u32) -> u32 {
+    self.weight_in_grams * cents_per_gram
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    #[should_panic]
-    fn fail_creating_weightless_package() {
-        let sender_country = String::from("Spain");
-        let recipient_country = String::from("Austria");
+  #[test]
+  #[should_panic]
+  fn fail_creating_weightless_package() {
+    let sender_country = String::from("Spain");
+    let recipient_country = String::from("Austria");
 
-        Package::new(sender_country, recipient_country, 5);
-    }
+    Package::new(sender_country, recipient_country, 5);
+  }
 
-    #[test]
-    fn create_international_package() {
-        let sender_country = String::from("Spain");
-        let recipient_country = String::from("Russia");
+  #[test]
+  fn create_international_package() {
+    let sender_country = String::from("Spain");
+    let recipient_country = String::from("Russia");
 
-        let package = Package::new(sender_country, recipient_country, 1200);
+    let package = Package::new(sender_country, recipient_country, 1200);
 
-        assert!(package.is_international());
-    }
+    assert!(package.is_international());
+  }
 
-    #[test]
-    fn create_local_package() {
-        let sender_country = String::from("Canada");
-        let recipient_country = sender_country.clone();
+  #[test]
+  fn create_local_package() {
+    let sender_country = String::from("Canada");
+    let recipient_country = sender_country.clone();
 
-        let package = Package::new(sender_country, recipient_country, 1200);
+    let package = Package::new(sender_country, recipient_country, 1200);
 
-        assert!(!package.is_international());
-    }
+    assert!(!package.is_international());
+  }
 
-    #[test]
-    fn calculate_transport_fees() {
-        let sender_country = String::from("Spain");
-        let recipient_country = String::from("Spain");
+  #[test]
+  fn calculate_transport_fees() {
+    let sender_country = String::from("Spain");
+    let recipient_country = String::from("Spain");
 
-        let cents_per_gram = 3;
+    let cents_per_gram = 3;
 
-        let package = Package::new(sender_country, recipient_country, 1500);
+    let package = Package::new(sender_country, recipient_country, 1500);
 
-        assert_eq!(package.get_fees(cents_per_gram), 4500);
-        assert_eq!(package.get_fees(cents_per_gram * 2), 9000);
-    }
+    assert_eq!(package.get_fees(cents_per_gram), 4500);
+    assert_eq!(package.get_fees(cents_per_gram * 2), 9000);
+  }
 }

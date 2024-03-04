7e57a86d176f82c2dce7c9e74b6aeadc736f2d15
Thuyen Code <thuyencode@proton.me>
2024-03-04 09:38:27 +0700

    Complete the 11's exercises

--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -11,34 +11,37 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+  let mut basket: HashMap<String, u32> = HashMap::new();
 
-    // Two bananas are already given for you :)
-    basket.insert(String::from("banana"), 2);
+  // Two bananas are already given for you :)
+  basket.insert(String::from("banana"), 2);
 
-    // TODO: Put more fruits in your basket here.
+  // TODO: Put more fruits in your basket here.
+  basket.insert(String::from("apple"), 2);
+  basket.insert(String::from("mango"), 2);
+  basket.insert(String::from("strawberry"), 2);
+  basket.insert(String::from("orange"), 2);
+  basket.insert(String::from("watermelon"), 2);
 
-    basket
+  basket
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn at_least_three_types_of_fruits() {
-        let basket = fruit_basket();
-        assert!(basket.len() >= 3);
-    }
+  #[test]
+  fn at_least_three_types_of_fruits() {
+    let basket = fruit_basket();
+    assert!(basket.len() >= 3);
+  }
 
-    #[test]
-    fn at_least_five_fruits() {
-        let basket = fruit_basket();
-        assert!(basket.values().sum::<u32>() >= 5);
-    }
+  #[test]
+  fn at_least_five_fruits() {
+    let basket = fruit_basket();
+    assert!(basket.values().sum::<u32>() >= 5);
+  }
 }
--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -14,73 +14,93 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
-    goals_scored: u8,
-    goals_conceded: u8,
+  goals_scored: u8,
+  goals_conceded: u8,
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
-    // The name of the team is the key and its associated struct is the value.
-    let mut scores: HashMap<String, Team> = HashMap::new();
+  // The name of the team is the key and its associated struct is the value.
+  let mut scores: HashMap<String, Team> = HashMap::new();
 
-    for r in results.lines() {
-        let v: Vec<&str> = r.split(',').collect();
-        let team_1_name = v[0].to_string();
-        let team_1_score: u8 = v[2].parse().unwrap();
-        let team_2_name = v[1].to_string();
-        let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded by team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
-    }
+  for r in results.lines() {
+    let v: Vec<&str> = r.split(',').collect();
+    let team_1_name = v[0].to_string();
+    let team_1_score: u8 = v[2].parse().unwrap();
+    let team_2_name = v[1].to_string();
+    let team_2_score: u8 = v[3].parse().unwrap();
+    // TODO: Populate the scores table with details extracted from the
+    // current line. Keep in mind that goals scored by team_1
+    // will be the number of goals conceded by team_2, and similarly
+    // goals scored by team_2 will be the number of goals conceded by
+    // team_1.
+
     scores
+      .entry(team_1_name)
+      .and_modify(|t: &mut Team| {
+        t.goals_scored += team_1_score;
+        t.goals_conceded += team_2_score
+      })
+      .or_insert(Team {
+        goals_scored: team_1_score,
+        goals_conceded: team_2_score,
+      });
+
+    scores
+      .entry(team_2_name)
+      .and_modify(|t: &mut Team| {
+        t.goals_scored += team_2_score;
+        t.goals_conceded += team_1_score
+      })
+      .or_insert(Team {
+        goals_scored: team_2_score,
+        goals_conceded: team_1_score,
+      });
+  }
+  scores
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    fn get_results() -> String {
-        let results = "".to_string()
-            + "England,France,4,2\n"
-            + "France,Italy,3,1\n"
-            + "Poland,Spain,2,0\n"
-            + "Germany,England,2,1\n";
-        results
-    }
+  fn get_results() -> String {
+    let results = "".to_string()
+      + "England,France,4,2\n"
+      + "France,Italy,3,1\n"
+      + "Poland,Spain,2,0\n"
+      + "Germany,England,2,1\n";
+    results
+  }
 
-    #[test]
-    fn build_scores() {
-        let scores = build_scores_table(get_results());
+  #[test]
+  fn build_scores() {
+    let scores = build_scores_table(get_results());
 
-        let mut keys: Vec<&String> = scores.keys().collect();
-        keys.sort();
-        assert_eq!(
-            keys,
-            vec!["England", "France", "Germany", "Italy", "Poland", "Spain"]
-        );
-    }
+    let mut keys: Vec<&String> = scores.keys().collect();
+    keys.sort();
+    assert_eq!(
+      keys,
+      vec!["England", "France", "Germany", "Italy", "Poland", "Spain"]
+    );
+  }
 
-    #[test]
-    fn validate_team_score_1() {
-        let scores = build_scores_table(get_results());
-        let team = scores.get("England").unwrap();
-        assert_eq!(team.goals_scored, 5);
-        assert_eq!(team.goals_conceded, 4);
-    }
+  #[test]
+  fn validate_team_score_1() {
+    let scores = build_scores_table(get_results());
+    let team = scores.get("England").unwrap();
+    assert_eq!(team.goals_scored, 5);
+    assert_eq!(team.goals_conceded, 4);
+  }
 
-    #[test]
-    fn validate_team_score_2() {
-        let scores = build_scores_table(get_results());
-        let team = scores.get("Spain").unwrap();
-        assert_eq!(team.goals_scored, 0);
-        assert_eq!(team.goals_conceded, 2);
-    }
+  #[test]
+  fn validate_team_score_2() {
+    let scores = build_scores_table(get_results());
+    let team = scores.get("Spain").unwrap();
+    assert_eq!(team.goals_scored, 0);
+    assert_eq!(team.goals_conceded, 2);
+  }
 }

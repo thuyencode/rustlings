24fb2cbf41201936527be460dd6e1c2b23914d85
Thuyen Code <thuyencode@proton.me>
2024-03-06 10:46:06 +0700

    Complete the 18's exercises

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -6,17 +6,15 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
-    let mut c = input.chars();
-    match c.next() {
-        None => String::new(),
-        Some(first) => ???,
-    }
+  let mut c = input.chars();
+  match c.next() {
+    None => String::new(),
+    Some(first) => first.to_uppercase().chain(c).collect(),
+  }
 }
 
 // Step 2.
@@ -24,7 +22,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+  words.iter().map(|w| capitalize_first(w)).collect()
 }
 
 // Step 3.
@@ -32,32 +30,32 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+  words.iter().map(|w| capitalize_first(w)).collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_success() {
-        assert_eq!(capitalize_first("hello"), "Hello");
-    }
+  #[test]
+  fn test_success() {
+    assert_eq!(capitalize_first("hello"), "Hello");
+  }
 
-    #[test]
-    fn test_empty() {
-        assert_eq!(capitalize_first(""), "");
-    }
+  #[test]
+  fn test_empty() {
+    assert_eq!(capitalize_first(""), "");
+  }
 
-    #[test]
-    fn test_iterate_string_vec() {
-        let words = vec!["hello", "world"];
-        assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
-    }
+  #[test]
+  fn test_iterate_string_vec() {
+    let words = vec!["hello", "world"];
+    assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+  }
 
-    #[test]
-    fn test_iterate_into_string() {
-        let words = vec!["hello", " ", "world"];
-        assert_eq!(capitalize_words_string(&words), "Hello World");
-    }
+  #[test]
+  fn test_iterate_into_string() {
+    let words = vec!["hello", " ", "world"];
+    assert_eq!(capitalize_words_string(&words), "Hello World");
+  }
 }
--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -3,40 +3,39 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+  // Complete this function to return the factorial of num
+  // Do not use:
+  // - return
+  // Try not to use:
+  // - imperative style loops (for, while)
+  // - additional variables
+  // For an extra challenge, don't use:
+  // - recursion
+  // Execute `rustlings hint iterators4` for hints.
+  (1..=num).fold(1, |acc, n| acc * n)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn factorial_of_0() {
-        assert_eq!(1, factorial(0));
-    }
+  #[test]
+  fn factorial_of_0() {
+    assert_eq!(1, factorial(0));
+  }
 
-    #[test]
-    fn factorial_of_1() {
-        assert_eq!(1, factorial(1));
-    }
-    #[test]
-    fn factorial_of_2() {
-        assert_eq!(2, factorial(2));
-    }
+  #[test]
+  fn factorial_of_1() {
+    assert_eq!(1, factorial(1));
+  }
+  #[test]
+  fn factorial_of_2() {
+    assert_eq!(2, factorial(2));
+  }
 
-    #[test]
-    fn factorial_of_4() {
-        assert_eq!(24, factorial(4));
-    }
+  #[test]
+  fn factorial_of_4() {
+    assert_eq!(24, factorial(4));
+  }
 }

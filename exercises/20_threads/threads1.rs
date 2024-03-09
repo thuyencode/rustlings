105144003d5d9c5389f0b64ea2c0f734d61cc386
Thuyen Code <thuyencode@proton.me>
2024-03-09 16:16:21 +0700

    Complete the 20's exercises

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -8,33 +8,31 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() {
-    let mut handles = vec![];
-    for i in 0..10 {
-        handles.push(thread::spawn(move || {
-            let start = Instant::now();
-            thread::sleep(Duration::from_millis(250));
-            println!("thread {} is complete", i);
-            start.elapsed().as_millis()
-        }));
-    }
+  let mut handles = vec![];
+  for i in 0..10 {
+    handles.push(thread::spawn(move || {
+      let start = Instant::now();
+      thread::sleep(Duration::from_millis(250));
+      println!("thread {} is complete", i);
+      start.elapsed().as_millis()
+    }));
+  }
 
-    let mut results: Vec<u128> = vec![];
-    for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
-    }
+  let mut results: Vec<u128> = vec![];
+  for handle in handles {
+    results.push(handle.join().unwrap());
+  }
 
-    if results.len() != 10 {
-        panic!("Oh no! All the spawned threads did not finish!");
-    }
+  if results.len() != 10 {
+    panic!("Oh no! All the spawned threads did not finish!");
+  }
 
-    println!();
-    for (i, result) in results.into_iter().enumerate() {
-        println!("thread {} took {}ms", i, result);
-    }
+  println!();
+  for (i, result) in results.into_iter().enumerate() {
+    println!("thread {} took {}ms", i, result);
+  }
 }

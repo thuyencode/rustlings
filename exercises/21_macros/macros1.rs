404549b682900de0a114efcf2c6d61a15d26d347
Thuyen Code <thuyencode@proton.me>
2024-03-09 16:21:52 +0700

    Complete the 21's exercises

--- exercises/21_macros/macros1.rs
+++ exercises/21_macros/macros1.rs
@@ -3,14 +3,12 @@
 // Execute `rustlings hint macros1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
-    () => {
-        println!("Check out my macro!");
-    };
+  () => {
+    println!("Check out my macro!");
+  };
 }
 
 fn main() {
-    my_macro();
+  my_macro!();
 }
